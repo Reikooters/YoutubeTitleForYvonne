72bf9382dee61913267635fc80a9b525fbed9a6f
Shane <shane@oraclecms.com>
2020-01-02 03:10:44 +1100

    Fixed typo in text shown at top of main screen

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -126,10 +126,10 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(949, 18);
+            this.label2.Size = new System.Drawing.Size(952, 18);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Open YouTube tab in Google Chrome first (non-minmized) and select Chrome window b" +
-    "elow. The left-most YouTube tab in the window is used.";
+            this.label2.Text = "Open YouTube tab in Google Chrome first (non-minimized) and select Chrome window " +
+    "below. The left-most YouTube tab in the window is used.";
             // 
             // pictureBox1
             // 
